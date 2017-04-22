fef445f31a198bcb9a8c33da779590f087337b38
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-04-22 17:23:05 +0100

    Made contract identify whether an address was stolen.

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -4,15 +4,26 @@ import "./Owned.sol";
 
 contract Identity is Owned {
 
-    mapping (address => string) public hashes;
+    struct RootHash {
+        string hash;
+        bool stolen;
+    }
+
+    mapping (address => RootHash) public hashes;
 
     event LogHashChanged(
         address indexed sender,
         string indexed previous,
         string indexed latest);
 
+    function markStolen(address who) onlyOwner {
+        hashes[msg.sender].stolen = true;
+    }
+
     function updateHash(string hash) {
-        LogHashChanged(msg.sender, hashes[msg.sender], hash);
-        hashes[msg.sender] = hash;
+        RootHash rootHash = hashes[msg.sender];
+        if (rootHash.stolen) throw;
+        LogHashChanged(msg.sender, rootHash.hash, hash);
+        rootHash.hash = hash;
     }
 }
