68b1aefacbd28fe531373062b1007e9ea6a81968
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-04-22 18:30:39 +0100

    Made it work for city hall.

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,58 @@
 "use strict";
 
+const Promise = require("bluebird");
+const Ipfs = require('ipfs-api');
+const DAG = require('ipld-dag-pb');
 const Web3 = require("web3");
 
+const ipfs = Ipfs('/ip4/127.0.0.1/tcp/5001');
 if (typeof web3 === "undefined") {
     var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
+Promise.promisifyAll(web3.eth, { suffix: "Promise" });
+Promise.promisifyAll(DAG.DAGLink, { suffix: "Promise" });
+Promise.promisifyAll(DAG.DAGNode, { suffix: "Promise" });
 
 const $ = require("jquery");
-const hospital = require("./hospital.js");
+const cityHall = require("./city_hall.js");
 
-require("file-loader?name=../hospital.html!../hospital.html");
+require("file-loader?name=../city_hall.html!../city_hall.html");
 
-console.log("Hello");
+console.log(DAG);
 $("#status").html("Loaded");
+
+$(".city_hall_sign").click(() => {
+    const address = $(".city_hall input[name='address']").val();
+    const dob = $(".city_hall input[name='dob']").val();
+    const nonce = $(".city_hall input[name='nonce']").val();
+    const value = JSON.stringify({
+        address: address,
+        dob: dob,
+        nonce: nonce
+    });
+    console.log(value);
+    let valueNode, sigNode, dobNode;
+    return DAG.DAGNode.createPromise(value)
+        .then(dagNode => {
+            valueNode = dagNode;
+            return ipfs.object.put(dagNode);
+        })
+        .then(dagNode => web3.eth.getAccountsPromise())
+        .then(accounts => web3.eth.signPromise(
+            accounts[ 0 ], web3.sha3(value)))
+        .then(signature => DAG.DAGNode.createPromise(signature))
+        .then(dagNode => {
+            sigNode = dagNode;
+            return ipfs.object.put(dagNode);
+        })
+        .then(dagNode => Promise.all([
+            DAG.DAGLink.createPromise("value", 1, valueNode.multihash),
+            DAG.DAGLink.createPromise("sig", 1, sigNode.multihash)
+        ]))
+        .then(links => DAG.DAGNode.createPromise("dob", links))
+        .then(dagNode => {
+            dobNode = dagNode;
+            return ipfs.object.put(dagNode);
+        })
+        .then(dagNode => console.log(dagNode.toJSON()));
+});
---
+++ ipfs_daemon.sh
@@ -0,0 +1,3 @@
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+ipfs daemon
